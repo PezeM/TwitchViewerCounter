9a8a0f60d6e513e895771a329bee959385a9a9b3
PezeM <pezemos@gmail.com>
2018-06-29 14:27:25 +0200

    First commit.

--- TwitchViewerCounter/Program.cs
+++ TwitchViewerCounter/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TwitchViewerCounter
 {
@@ -10,6 +6,7 @@ namespace TwitchViewerCounter
     {
         static void Main(string[] args)
         {
+            Console.ReadKey();
         }
     }
 }
