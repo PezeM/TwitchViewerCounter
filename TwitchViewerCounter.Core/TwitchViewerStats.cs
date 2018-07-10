89452e50d92589fa48b4ecda60acff2818194f3b
PezeM <pezemos@gmail.com>
2018-07-10 18:43:17 +0200

    Added timers to check if stream goes online.

--- TwitchViewerCounter.Core/TwitchViewerStats.cs
+++ TwitchViewerCounter.Core/TwitchViewerStats.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
 using System.Threading.Tasks;
 using TwitchViewerCounter.Core.Configuration;
 using TwitchViewerCounter.Core.Exceptions;
 using TwitchViewerCounter.Core.Models;
 using TwitchViewerCounter.Core.RequestHandler;
+using System.Collections.Specialized;
 
 namespace TwitchViewerCounter.Core
 {
@@ -11,6 +15,7 @@ namespace TwitchViewerCounter.Core
     {
         private TwitchApiRequestHandler TwitchApi { get; set; }
         private TMIApiRequestHandler TMIApi { get; set; }
+        private ObservableCollection<string> OnlineLiveStreams { get; set; }
 
         public async Task Start(string clientId)
         {
@@ -18,9 +23,26 @@ namespace TwitchViewerCounter.Core
 
             TwitchApi = new TwitchApiRequestHandler(clientId);
             TMIApi = new TMIApiRequestHandler();
+            OnlineLiveStreams = new ObservableCollection<string>();
+            OnlineLiveStreams.CollectionChanged += OnlineLiveStreams_CollectionChanged;
 
+            SetupTimers();
+        }
+
+        private void SetupTimers()
+        {
             var liveStreamsCheckList = TwitchViewerCounterConfiguration.Instance.GetLiveStreamsList();
-            var liveStreams = await TwitchApi.GetLiveStreamsInformationAsync(liveStreamsCheckList.ToArray());
+            var liveCheckInterval = TwitchViewerCounterConfiguration.Instance.GetCheckInterval();
+
+            // Runs only if the check list is declared
+            if (liveStreamsCheckList != null)
+            {
+                // Timer that will check list of live streams to see their status
+                var checkLiveStreamsStatusTimer = new Timer(e => CheckLiveStreamsStatusAsync(liveStreamsCheckList),
+                    null, TimeSpan.Zero, TimeSpan.FromSeconds(liveCheckInterval));
+
+                var getLiveStreamsInformationTimer = new Timer(e => GetLiveStreamsInformation(OnlineLiveStreams), null, TimeSpan.Zero, TimeSpan.FromSeconds(60));
+            }
         }
 
         public async Task GetViewersInfoAsync(string channelName)
@@ -98,5 +120,42 @@ namespace TwitchViewerCounter.Core
                 throw new ClientIdNotSetException(message);
             }
         }
+
+        private void OnlineLiveStreams_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            Logger.Log("Running on live streams collection changed...");
+            if (e.NewItems != null)
+            {
+                foreach (var item in e.NewItems)
+                {
+                    Logger.Log($"User went live: {item}");
+                }
+            }
+        }
+
+        private async Task GetLiveStreamsInformation(ObservableCollection<string> onlineLiveStreams)
+        {
+            Logger.Log("Running get live streams information...");
+            if (onlineLiveStreams.Count != 0)
+            {
+                foreach (var live in onlineLiveStreams)
+                {
+                    await GetViewersInfoAsync(live);
+                }
+            }
+        }
+
+        private async Task CheckLiveStreamsStatusAsync(List<string> liveStreamsCheckList)
+        {
+            var liveStreams = await TwitchApi.GetLiveStreamsInformationAsync(liveStreamsCheckList.ToArray());
+            Logger.Log("Running check live streams status...");
+            foreach (var live in liveStreams.StreamsInfo)
+            {
+                if (IsLiveOnline(live) && !OnlineLiveStreams.Contains(live.Channel.Name))
+                    OnlineLiveStreams.Add(live.Channel.Name);
+            }
+        }
+
+        private bool IsLiveOnline(Stream live) => live.StreamType == "live";
     }
 }
