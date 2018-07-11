bf2e5ee8c0e9cbe5cac091f63b80042fb21c878c
PezeM <pezemos@gmail.com>
2018-07-12 01:37:48 +0200

    Added one basic test.

---
+++ TwitchViewerCounter.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TwitchViewerCounter.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TwitchViewerCounter.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bf5da2fe-2614-4ce3-8902-63d6673b32c4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TwitchViewerCounter.Tests/TMIApiTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using TwitchViewerCounter.Core.Constans;
+using TwitchViewerCounter.Core.Models;
+
+namespace TwitchViewerCounter.Tests
+{
+    [TestClass]
+    public class TMIApiTest
+    {
+        [TestMethod]
+        public void GetChatterResponseTest()
+        {
+            var client = new RestClient(ReguestConstans.TMIApiUrl);
+            var request = new RestRequest("lirik/chatters", Method.GET);
+
+            var response = client.Execute<TMIRequestResponse>(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Assert.IsNotNull(response.Content);
+            }
+            else
+            {
+                Assert.Fail(response.StatusDescription);
+            }
+        }
+    }
+}
