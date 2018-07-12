7037fee5843bb6a22def99dd54fd3500842d68b8
PezeM <pezemos@gmail.com>
2018-07-12 16:45:58 +0200

    Created MongoDataContext class.
    Added connection string inside App.config.

---
+++ TwitchViewerCounter.Database/MongoDataContext.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Driver;
+using System.Configuration;
+
+namespace TwitchViewerCounter.Database
+{
+    public class MongoDataContext
+    {
+        public IMongoDatabase MongoDatabase { get; }
+
+        public MongoDataContext() : this("MongoDb")
+        {
+        }
+
+        public MongoDataContext(string connectionName)
+        {
+            var url = ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+
+            var mongoUrl = new MongoUrl(url);
+            IMongoClient client = new MongoClient(mongoUrl);
+            MongoDatabase = client.GetDatabase(mongoUrl.DatabaseName);
+        }
+
+    }
+}
